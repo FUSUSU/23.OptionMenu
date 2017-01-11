f261c7c0363e44bf136c89e02928295a27dfaebe
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-11 21:30:33 +0700

    Click vào từng Item hoàn thành

--- app/src/main/java/com/example/teo/a23optionmenu/MainActivity.java
+++ app/src/main/java/com/example/teo/a23optionmenu/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.teo.a23optionmenu;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Menu Item
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity, menu); //Menu Resource, Menu
+        return true;
+    }
+
+
+    //Click vào từng Item nó sẽ xuất hiến những cái này.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(), "Item 1 Selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(),"Item 2 Selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(),"Item 3 Selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
